5404bcc7c5bcb95e4217340d8f291517483bad7a
FerminDRA <fermindelrosarioantonio@gmail.com>
2022-09-02 10:29:46 -0500

    create work_experience #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+    let work1 ={
+            fecha: "2019-2020",
+            ubicacion:"Orizaba, Ver",
+            puesto: "Java Developer",
+            empresa: "Desp",
+            logros : [
+                      {descripcion : "Experto en java"},
+                      {descripcion : "Creacion de framework"}
+            ]
+    };
+    let work2 ={
+            fecha: "2020-2022",
+            ubicacion:"Cordoba, Ver",
+            puesto: "CEO",
+            empresa: "Losit",
+            logros : [
+                      {descripcion : "IA"},
+                      {descripcion : "Sistema automotriz"}
+                    ]
+    };
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
   }
 
 }
